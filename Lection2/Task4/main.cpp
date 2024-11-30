44f18a160a236680c500ead7895c37ba633125f6
EvGoncharenko <Kilimangaro99@Gmail.com>
2024-12-01 01:19:13 +0200

    Added mi fourth task of the second lection

--- Lection2/Task4/main.cpp
+++ Lection2/Task4/main.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main()
 {
-    char user_string [256]{0};
-    int counter = 0;
+    string user_string;
+    map<char, int> m_string;
 
-    cout << "Enter your string. No more than 256 characters: ";
-    cin.getline(user_string,256);
+    cout << "Enter your string: ";
+    cin >> user_string;
 
     cout << "Your string: " << user_string << endl;
 
-    for(char symbol = ' '; symbol < '|' + 1; symbol++){
-        for(char user_string_element:user_string){
-            if(symbol == user_string_element){
-                counter++;
-            }else{
-                continue;
-            }
-        }
-        if(counter > 0){
-            cout << "Symbols per string '" << symbol << "' - " << counter << endl;
-            counter = 0;
-        }
+    for(char symbol:user_string){
+        m_string[symbol]++;
+    }
+
+    for(auto &[symbol, amount]:m_string){
+        cout << "Symbol '"<< symbol << "' " << "= " << amount << endl;
     }
 }
